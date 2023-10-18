074fc56add4efd8e73428a5c22b328ac9f44ca20
azamdzobair <azamdzobair@gmail.com>
2023-10-18 22:09:23 +0600

    Edit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,13 +8,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 const About = () => {
     return (
-        <div className='mt-24'>
-            <div className='mx-auto px-24'>
+        <div className='mt-14'>
+            <div className='mx-auto px-6'>
                 <div>
-                    <h1 className=' text-4xl text-center font-bold mb-5 text-gray-800'>Introduction About Our Business Firm </h1>
-                    <p className='text-center mx-60 text-gray-800'>Aenean purus esse habitant facilisi rhoncus metus aut sodales voluptas veniam aute, incidunt semper iusto? Praesent! Lobortis? Tellus ipsa. Laborum. Platea, nec! Ullam repellat aenean feugiat, tempor.</p>
+                    <h1 className='text-2xl text-center font-bold mb-5 text-gray-800'>Introduction About Our Business Firm </h1>
+                    <p className='text-center  text-gray-800'>Aenean purus esse habitant facilisi rhoncus metus aut sodales voluptas veniam aute, incidunt semper iusto? Praesent! Lobortis? Tellus ipsa. Laborum. Platea, nec! Ullam repellat aenean feugiat, tempor.</p>
                 </div>
-                <div className='mt-16 flex items-center  '>
+                <div className='mt-14 sm:flex items-center '>
                     <img src={img02} />
                     <div className='bg-amber-500 w-full p-10'>
                         <div className=' flex items-center'>
@@ -29,60 +29,65 @@ const About = () => {
                         <Accordion></Accordion>
                     </div>
                 </div>
-                <div className='flex justify-center mt-16'>
+                <div className='flex justify-center mt-10'>
                     <button className='bg-amber-500 py-2 px-4 text-lg font-medium text-white hover:bg-orange-700 duration-500 ease-in '>More About Us</button>
                 </div>
             </div>
-            <div className='flex  mt-24'>
+            <div className='  mt-14'>
+                <div>
+                    <h1>Hello</h1>
+                </div>
 
-                <div className='w-1/2 relative '>
-                    <img className='absolute' src={img04} />
-                    <div className=' bg-gray-900 bg-opacity-50 w-full h-[450px]  absolute py-12 px-24'>
-                        <div className='mb-6 text-white'>
-                            <h1 className='text-4xl  font-bold mb-3'>Hire Us For Any Types Of Business Related Services !</h1>
-                            <p>Hic voluptatibus asperiores animi vero. Felis ipsum. Gravida aspernatur adipisci mus adipisci ipsam etiam, repellendus aptent aliquam quas, aptent ab curabitu.</p>
-                        </div>
-                        <div className='flex'>
-                            <div className='w-1/2 mb-4'>
-                                <h1 className='text-xl text-white font-bold mr-6 '>Monday - Friday 9:00 AM - 6:00 pm Sunday & Public Holidays (Closed)
-                                </h1>
+                {/* <div className=' w-full sm:w-1/2  '>
+                    <div className='relative'>
+                        <img className='absolute h-[480px]' src={img04} />
+                        <div className=' bg-gray-900 bg-opacity-50 w-full h-[480px]  absolute py-12 px-6'>
+                            <div className='mb-6 text-white'>
+                                <h1 className='text-3xl  font-bold mb-3'>Hire Us For  Business Related Services !</h1>
+                                <p>Hic voluptatibus asperiores animi vero. Felis ipsum. Gravida aspernatur adipisci mus adipisci ipsam etiam, repellendus aptent aliquam quas, aptent ab curabitu.</p>
                             </div>
-                            <div className='text-white'>
-                                <div className='flex items-center mb-2 '>
-                                    <FontAwesomeIcon icon={faCircleCheck} />
-                                    <p className='ml-2'>Deleniti, aliquid nullam.</p>
+                            <div className='flex'>
+                                <div className='w-1/2 mb-6'>
+                                    <h1 className='text-xl text-white font-bold mr-6 '>Monday - Friday 9:00 AM - 6:00 pm Sunday & Public Holidays (Closed)
+                                    </h1>
                                 </div>
-                                <div className='flex items-center mb-2 '>
-                                    <FontAwesomeIcon icon={faCircleCheck} />
-                                    <p className='ml-2'>Deleniti, aliquid nullam.</p>
-                                </div>
-                                <div className='flex items-center mb-2 '>
-                                    <FontAwesomeIcon icon={faCircleCheck} />
-                                    <p className='ml-2'>Deleniti, aliquid nullam.</p>
-                                </div>
-                                <div className='flex items-center mb-2 '>
-                                    <FontAwesomeIcon icon={faCircleCheck} />
-                                    <p className='ml-2'>Deleniti, aliquid nullam.</p>
-                                </div>
+                                <div className='text-white'>
+                                    <div className='flex items-center mb-2 '>
+                                        <FontAwesomeIcon icon={faCircleCheck} />
+                                        <p className='ml-2'>Deleniti, aliquid nullam.</p>
+                                    </div>
+                                    <div className='flex items-center mb-2 '>
+                                        <FontAwesomeIcon icon={faCircleCheck} />
+                                        <p className='ml-2'>Deleniti, aliquid nullam.</p>
+                                    </div>
+                                    <div className='flex items-center mb-2 '>
+                                        <FontAwesomeIcon icon={faCircleCheck} />
+                                        <p className='ml-2'>Deleniti, aliquid nullam.</p>
+                                    </div>
+                                    <div className='flex items-center mb-2 '>
+                                        <FontAwesomeIcon icon={faCircleCheck} />
+                                        <p className='ml-2'>Deleniti, aliquid nullam.</p>
+                                    </div>
 
+                                </div>
                             </div>
+                            <button className='bg-amber-500 py-2 px-4 text-lg font-medium text-white hover:bg-orange-700 duration-500 ease-in '>HIRE US NOW!</button>
                         </div>
-                        <button className='bg-amber-500 py-2 px-4 text-lg font-medium text-white hover:bg-orange-700 duration-500 ease-in '>HIRE US NOW!</button>
                     </div>
 
 
 
-                </div>
-                <div className='w-1/2 relative flex items-center'>
-                    <img className='' src={img08} />
-                    <div className=' absolute '>
+                </div> */}
+                <div className=' w-full sm:w-1/2 '>
+                    <img className='h-[480px]' src={img08} />
+                    {/* <div className=' absolute '>
                         <div className='bg-white w-48 h-40'>
 
                         </div>
                         <div className='bg-amber-500 w-48 h-40'>
 
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
--- src/Components/ServiceDetails/ServiceCard.js
+++ src/Components/ServiceDetails/ServiceCard.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+
+
 const ServiceCard = ({ cardData }) => {
     return (
         <div>
-            <div className='' >
+
+
+
+            <div className='  bg-amber-600 ml-4 p-4' >
                 <img className='w-16 h-16 ' src={cardData.icon} />
 
-                <h1 className='text-lg font-bold text-gray-800 my-2'>{cardData.title}</h1>
-                <p className=' text-gray-800  '>{cardData.des}</p>
+                <h1 className='text-lg font-bold  text-white my-2'>{cardData.title}</h1>
+                <p className=' text-white '>{cardData.des}</p>
 
             </div>
-        </div>
+
+
+
+
+
+
+        </div >
     );
 };
 
