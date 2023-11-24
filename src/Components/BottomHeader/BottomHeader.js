7120bce7aff4c5a721cba636b8da5e95236bbfac
azamdzobair <azamdzobair@gmail.com>
2023-11-24 11:35:54 +0600

    edit menu

--- src/Components/BottomHeader/BottomHeader.js
+++ src/Components/BottomHeader/BottomHeader.js
@@ -27,7 +27,7 @@ const BottomHeader = () => {
         { id: 6, name: 'CONTACT', link: 'contact' },
     ]
     return (
-        <nav className='bg-blue-500 sticky  relative h-14 z-10 '>
+        <nav className='bg-blue-500   relative h-14 z-10 '>
             <div className=' container mx-auto px-6  flex items-center justify-between '>
                 <a className='text-2xl  font-bold text-white ' href='#'>AZADI</a>
 
@@ -45,7 +45,7 @@ const BottomHeader = () => {
 
 
 
-                    <ul className={`absolute bg-blue-700 w-full  top-10 right-0 p-4 text-center duration-500 ease-in list-none  ${open ? 'top-14' : 'hidden'} sm:h-14 sm:block sm:relative sm:top-0 sm:flex sm:items-center `}>
+                    <ul className={`absolute bg-blue-700 w-full  top-10 right-0 p-4 text-center duration-500 ease-in list-none  ${open ? 'top-14' : 'hidden'} sm:h-14  sm:relative sm:top-0 sm:flex sm:items-center `}>
                         {
                             routes.map(route => <Linkk key={route.id}
 
